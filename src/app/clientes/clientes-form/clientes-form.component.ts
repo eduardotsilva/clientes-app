169b736912d9caffdecd330e983c61c9aae2b2d5
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-17 20:54:18 -0300

    57. Populando o cliente através do formulário

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Cliente } from '../cliente';
+import { Component, OnInit } from "@angular/core";
+import { Cliente } from "../cliente";
 
 @Component({
-  selector: 'app-clientes-form',
-  templateUrl: './clientes-form.component.html',
-  styleUrls: ['./clientes-form.component.css']
+  selector: "app-clientes-form",
+  templateUrl: "./clientes-form.component.html",
+  styleUrls: ["./clientes-form.component.css"],
 })
 export class ClientesFormComponent implements OnInit {
-
   cliente: Cliente;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.cliente = new Cliente();
   }
+
+  ngOnInit() {}
 
 }
--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,17 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { ClientesRoutingModule } from './clientes-routing.module';
-import { ClientesFormComponent } from './clientes-form/clientes-form.component';
+import { ClientesRoutingModule } from "./clientes-routing.module";
+import { ClientesFormComponent } from "./clientes-form/clientes-form.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [ClientesFormComponent],
-  imports: [
-    CommonModule,
-    ClientesRoutingModule
-  ],
-  exports:[
-    ClientesFormComponent
-  ]
+  imports: [CommonModule, ClientesRoutingModule, FormsModule],
+  exports: [ClientesFormComponent],
 })
-export class ClientesModule { }
+export class ClientesModule {}
