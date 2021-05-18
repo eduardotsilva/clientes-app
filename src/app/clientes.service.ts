76536f59bd7c9c3c2e1730fbd1cc39ec5cf72292
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-17 21:46:13 -0300

    62. CORS: Salvando cliente através da aplicação web para a REST API

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from "@angular/core";
 import { Cliente } from "./clientes/cliente";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
 })
 export class ClientesService {
-  constructor(private http: HttpClient) {
-    
-  }
+  constructor(private http: HttpClient) {}
 
   getCliente(): Cliente {
     let cliente: Cliente = new Cliente();
@@ -16,5 +15,12 @@ export class ClientesService {
     cliente.cpf = "1231231";
 
     return cliente;
+  }
+
+  salvar(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(
+      "http://localhost:8080/api/clientes",
+      cliente
+    );
   }
 }
