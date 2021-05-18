81c84d865788ceb19adb459d88bf5272be8971d7
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-18 20:21:32 -0300

    67. Listando os clientes através da API

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -16,17 +16,7 @@ export class ClientesService {
     );
   }
 
-  /*
   getClientes(): Observable<Cliente[]> {
-    return null;
-  }
-*/
-  getClientes(): Cliente[] {
-    let cliente = new Cliente();
-    cliente.id  = "1";
-    cliente.nome = "Eduardo";
-    cliente.cpf = "1231231";
-    cliente.dataCadastro = "28/12/1990"
-    return [cliente];
+    return this.http.get<Cliente[]>("http://localhost:8080/api/clientes");
   }
 }
--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { ClientesService } from '../../clientes.service';
-import { Cliente } from '../cliente';
+import { Component, OnInit } from "@angular/core";
+import { ClientesService } from "../../clientes.service";
+import { Cliente } from "../cliente";
 
 @Component({
-  selector: 'app-clientes-lista',
-  templateUrl: './clientes-lista.component.html',
-  styleUrls: ['./clientes-lista.component.css']
+  selector: "app-clientes-lista",
+  templateUrl: "./clientes-lista.component.html",
+  styleUrls: ["./clientes-lista.component.css"],
 })
 export class ClientesListaComponent implements OnInit {
-
   clientes: Cliente[] = [];
 
-  constructor(private service:ClientesService) { }
+  constructor(private service: ClientesService) {}
 
   ngOnInit() {
-    this.clientes = this.service.getClientes();
+    this.service.getClientes().subscribe(
+      (response) => {
+        this.clientes = response;
+      },
+      (responseError) => {}
+    );
   }
-
 }
