104db2c17b8750a1ee9c8156ea230bf3db96d99a
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-22 19:04:53 -0300

    79. Formulário de serviços prestados. parte 1

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Cliente } from "../../clientes/cliente";
+import { ClientesService } from "../../clientes.service";
 
 @Component({
-  selector: 'app-servico-prestado-form',
-  templateUrl: './servico-prestado-form.component.html',
-  styleUrls: ['./servico-prestado-form.component.css']
+  selector: "app-servico-prestado-form",
+  templateUrl: "./servico-prestado-form.component.html",
+  styleUrls: ["./servico-prestado-form.component.css"],
 })
 export class ServicoPrestadoFormComponent implements OnInit {
+  clientes: Cliente[] = [];
 
-  constructor() { }
+  constructor(private clienteService: ClientesService) {}
 
   ngOnInit() {
+    this.clienteService.getClientes().subscribe((response) => {
+      this.clientes = response;
+    });
   }
 
+  onSubmit() {
+    console.log("submit");
+  }
 }
--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ServicoPrestadoFormComponent } from "./servico-prestado-form/servico-prestado-form.component";
+import { ServicoPrestadoListaComponent } from "./servico-prestado-lista/servico-prestado-lista.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "servico-prestado-form", component: ServicoPrestadoFormComponent },
+  { path: "servico-prestado-lista", component: ServicoPrestadoListaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ServicoPrestadoRoutingModule { }
+export class ServicoPrestadoRoutingModule {}
