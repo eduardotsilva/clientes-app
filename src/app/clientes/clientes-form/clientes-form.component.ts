cdb010d6a658968777a493eb59916e93ed5ac6fb
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-24 20:00:08 -0300

    92. Trabalhando com rotas filhas e redirect To

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -70,7 +70,7 @@ export class ClientesFormComponent implements OnInit {
   }
 
   voltarParaListagem() {
-    this.router.navigate(["/clientes-lista"]);
+    this.router.navigate(["/clientes/lista"]);
   }
 
  
--- src/app/servico-prestado/servico-prestado-routing.module.ts
+++ src/app/servico-prestado/servico-prestado-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { LayoutComponent } from "../template/layout/layout.component";
 import { ServicoPrestadoFormComponent } from "./servico-prestado-form/servico-prestado-form.component";
 import { ServicoPrestadoListaComponent } from "./servico-prestado-lista/servico-prestado-lista.component";
 
 const routes: Routes = [
-  { path: "servico-prestado-form", component: ServicoPrestadoFormComponent },
-  { path: "servico-prestado-lista", component: ServicoPrestadoListaComponent },
+  {
+    path: "servicos-prestados",
+    component: LayoutComponent,
+    children: [
+      {
+        path: "form",
+        component: ServicoPrestadoFormComponent,
+      },
+      {
+        path: "lista",
+        component: ServicoPrestadoListaComponent,
+      },
+
+      { path: "", redirectTo: "/servicos-prestados/lista", pathMatch: "full" },
+    ],
+  },
 ];
 
 @NgModule({
