d22094ffa3b7e2b85e28c0bf50b5ed034b0ccfa6
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-24 19:23:48 -0300

    90. Finalizando a tela de login e registro

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,32 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"],
 })
-export class LoginComponent  {
+export class LoginComponent {
   username: string;
   password: string;
   loginError: boolean;
+  cadastroUsuario: boolean;
 
-  constructor() {}
-
+  constructor(
+    private router:Router
+  ) {}
 
   onSubmit() {
     console.log(`User: ${this.username}, pass: ${this.password}`);
+    this.router.navigate(['/home'])
+  }
+
+  cadastrarUsuario(event) {
+    event.preventDefault();
+    this.cadastroUsuario = true;
+  }
+
+  cancelaCadastro() {
+    this.cadastroUsuario = false;
   }
 }
