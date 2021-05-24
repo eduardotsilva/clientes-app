1b3e75bcdd650bac7bbbeb6ee403b8a9ddb9a96c
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-24 19:46:19 -0300

    91. Separando o login do template da aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+import { LayoutComponent } from "./template/layout/layout.component";
 
 const routes: Routes = [
-  
-  { path: 'login', component: LoginComponent},
-  { path: 'home', component: HomeComponent}
+  { path: "login", component: LoginComponent },
 
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [{ path: "home", component: HomeComponent }],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent} from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component'
 import { RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidebarComponent],
+  declarations: [NavbarComponent, SidebarComponent, LayoutComponent],
   imports: [
     CommonModule,
     RouterModule
