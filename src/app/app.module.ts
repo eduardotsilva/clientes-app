1602689814bfca680a964c6c94df8ea584781cd8
NoteEdu <eduardo.tsilva@hotmail.com>
2021-05-24 19:05:42 -0300

    89. Criando o formulário de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,30 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { ClientesModule } from "./clientes/clientes.module";
-
 import { TemplateModule } from "./template/template.module";
 import { HomeComponent } from "./home/home.component";
 import { ClientesService } from "./clientes.service";
-import { HttpClientModule } from "@angular/common/http";
 import { ServicoPrestadoModule } from "./servico-prestado/servico-prestado.module";
 import { ServicoPrestadoService } from "./servico-prestado.service";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent,LoginComponent],
 
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     ClientesModule,
     TemplateModule,
-    ServicoPrestadoModule
+    ServicoPrestadoModule,
   ],
-  providers: [ClientesService,ServicoPrestadoService],
+  providers: [ClientesService, ServicoPrestadoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
