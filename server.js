ccb9c52de8a4a6c143c38d741a42fa84a92ce4ad
NoteEdu <eduardo.tsilva@hotmail.com>
2021-06-01 20:29:50 -0300

    configuração para publicação no Heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path =  require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+
+app.use(express.static(__dirname + '/dist/clientes-app'));
+
+app.get('/*', (req,res) => {res.sendFile(__dirname + '/dist/clientes-app/index.html')});
+
+app.listen(PORT, () => {
+    console.log('Servidor iniciado na porta ' + PORT);
+})
